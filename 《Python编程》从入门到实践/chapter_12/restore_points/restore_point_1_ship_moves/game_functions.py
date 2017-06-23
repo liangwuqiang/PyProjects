a58c82bced893641693d30b079402b622757b796
liangwuqiang <l_wuqiang@126.com>
2017-06-23 16:47:20 +0800

    a

--- 《Python编程》从入门到实践/chapter_12/restore_points/restore_point_1_ship_moves/game_functions.py
+++ 《Python编程》从入门到实践/chapter_12/restore_points/restore_point_1_ship_moves/game_functions.py
@@ -4,21 +4,23 @@ import pygame
 
 
 def check_keydown_events(event, ship):
-    """Respond to keypresses."""
+    """响应按键按下"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
         
+
 def check_keyup_events(event, ship):
-    """Respond to key releases."""
+    """响应按键弹起"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
+
 def check_events(ship):
-    """Respond to keypresses and mouse events."""
+    """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -27,10 +29,11 @@ def check_events(ship):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
+
 def update_screen(ai_settings, screen, ship):
-    """Update images on the screen and flip to the new screen."""
-    # Redraw the screen during each pass through the loop.
+    """更新屏幕图像，弹到新屏幕"""
+    # 重画屏幕，在每次过场循环中
     screen.fill(ai_settings.bg_color)
     ship.blitme()
-    # Make the most recently drawn screen visible.
+    # 使最近回执的屏幕可见
     pygame.display.flip()
